402157e9e0d6f5bee76f5b30ede5a00b128d4be8
snndesign <snndesign@gmail.com>
2025-11-28 16:50:04 +0200

    fix: correct entry point for multi-page

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// vite.config.js   ←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←←
-const { defineConfig } = require('vite')
-const react = require('@vitejs/plugin-react')
-const path = require('path')
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main:     path.resolve(__dirname, 'index.html'),
-        private:  path.resolve(__dirname, 'private.html'),
+        main: path.resolve(__dirname, 'index.html'),
+        private: path.resolve(__dirname, 'private.html'),
         business: path.resolve(__dirname, 'business.html'),
         contract: path.resolve(__dirname, 'contract.html'),
-        payment:  path.resolve(__dirname, 'payment.html'),
-        wifi:     path.resolve(__dirname, 'wifi.html'),
-        quality:  path.resolve(__dirname, 'quality.html'),
-        iptv:     path.resolve(__dirname, 'iptv.html'),
+        payment: path.resolve(__dirname, 'payment.html'),
+        wifi: path.resolve(__dirname, 'wifi.html'),
+        quality: path.resolve(__dirname, 'quality.html'),
+        iptv: path.resolve(__dirname, 'iptv.html'),
       }
     }
   }
